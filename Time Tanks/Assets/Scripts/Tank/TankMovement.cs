f3d2016df9d845d84fe04b9f7210d6ad5ddc2d42
Jishaxe <josh@joshbe.me>
2019-05-08 14:46:22 +0100

    texture improvements and added wheels

--- Time Tanks/Assets/Scripts/Tank/TankMovement.cs
+++ Time Tanks/Assets/Scripts/Tank/TankMovement.cs
@@ -5,6 +5,14 @@ using UnityEngine;
 public class TankMovement : MonoBehaviour
 {
     Tank tank;
+    MovementControl currentMovement;
+
+    public Rigidbody rb;
+
+
+    public float forwardSpeed;
+    public float reverseSpeed;
+    public float turnSpeed;
 
     private void Awake()
     {
@@ -14,6 +22,31 @@ public class TankMovement : MonoBehaviour
 
     public void OnMovementChanged(MovementControl oldMovement, MovementControl newMovement)
     {
+        currentMovement = newMovement;
+
+        if (currentMovement.forwards)
+        {
+           rb.AddForce(transform.forward * forwardSpeed * Time.deltaTime, ForceMode.Acceleration);
+        }
+
+        if (currentMovement.reverse)
+        {
+            rb.AddForce(-transform.forward * reverseSpeed * Time.deltaTime, ForceMode.Acceleration);
+        }
+
+        if (currentMovement.right)
+        {
+            rb.AddRelativeTorque(Vector3.up * turnSpeed * Time.deltaTime, ForceMode.VelocityChange);
+        }
+
+        if (currentMovement.left)
+        {
+            rb.AddRelativeTorque(Vector3.up * -turnSpeed * Time.deltaTime, ForceMode.VelocityChange);
+        }
+    }
+
+    public void Update()
+    { 
 
     }
 }
